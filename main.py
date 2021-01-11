9330672bd97c066a03c4ff8e1cbd3fce89ce6e5b
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2021-01-11 17:56:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+
+def square(a):
+  """item_exporters.py contains Scrapy item exporters.
+
+Once you have scraped your items, you often want to persist or export those items, to use the data in some other
+application. That is, after all, the whole purpose of the scraping process.
+
+For this purpose Scrapy provides a collection of Item Exporters for different output formats, such as XML, CSV or JSON.
+
+More Info:
+    https://doc.scrapy.org/en/latest/topics/exporters.html
+
+"""
+  return a**a
+
+
 x = 1
 if x == 1:
     # indented four spaces
